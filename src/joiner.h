dc320c490c29f56bd4a1da3d5b8788d8fac65c42
wojciech-pawlik <wojciech95pawlik@gmail.com>
2022-03-21 21:19:40 +0100

    Deal with quotations, commas and newlines, test csvs

---
+++ src/joiner.h
@@ -0,0 +1,21 @@
+#ifndef WICKED_CSV_JOINER_H
+#define WICKED_CSV_JOINER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE_LEN 32768
+int QUOTATION_COUNTER;
+
+int get_col_pos(char* header_line, const char* col_name);
+int get_col_cnt(char* header_line);
+void fill_row(char* line, char* join_type, int col_cnt);
+char* get_line(int max_len, FILE *stream);
+char* get_val(char* line, int pos);
+void remove_newline_char(char* line);
+int join_line(char* left_line, char* right_path, int left_col_pos, int right_col_pos, char* join_type);
+int join_header(char* left_header, char* right_header);
+int join(char* left_path, char* right_path, char* col, char* join_type);
+
+#endif //WICKED_CSV_JOINER_H
---
+++ src/main.c
@@ -0,0 +1,38 @@
+#include "joiner.h"
+
+int main(int argc, char** argv) {
+    int status;
+
+    if(argc < 5)
+    {
+        printf("Too few arguments.\n");
+        return -1;
+    }
+    if(strcmp(argv[1], "join") != 0)
+    {
+        printf("Method %s not specified. You can use \"join\" method only in this demo version.\n", argv[1]);
+        return -2;
+    }
+    char* left_path = argv[2];
+    char* right_path = argv[3];
+    char* column_name = argv[4];
+
+    char* join_type;
+    if(argc == 5)
+    {
+        printf("Join type not specified. Using default \"inner join\" method.\n");
+        join_type = "inner";
+    }
+    else
+    {
+        join_type = argv[5];
+        if(strcmp(join_type, "inner") != 0 && strcmp(join_type, "left") != 0 && strcmp(join_type, "right") != 0)
+        {
+            printf("Join type not specified correctly: \"%s\" instead of inner, left, or right. Using default \"inner join\" method.\n", join_type);
+            join_type = "inner";
+        }
+    }
+
+    status = join(left_path, right_path, column_name, join_type);
+    return status;
+}
